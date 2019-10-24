58ed4d1f6cb5efbbfaad6b0fab63c67d1b693b0c
Abdullah <abdullah.khan@brainplow.com>
2019-10-24 18:31:32 +0500

    trurur

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,8 +5,8 @@ export class Config {
   
   // public static api: String = 'https://influexpapi.herokuapp.com';
   // public static api: String = 'http://ns520442.ip-158-69-23.net:4433';
-  public static api: String = 'http://192.168.29.166:8000';
-    // public static api: String = 'https://apis.influexpai.com';
+  // public static api: String = 'http://192.168.29.166:8000';
+    public static api: String = 'https://apis.influexpai.com';
     // public static api: String = 'https://devapis.influexpai.com';
     public  static  Imageurlupload = 'https://storage.influexpai.com/test_hamza.php';
 
